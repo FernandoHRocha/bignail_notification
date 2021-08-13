bc5900a3bf978d125e4ce4d4bf1dd0abc7630008
= <=>
2021-08-13 13:38:16 -0300

    Refeita a organização

--- bot.py
+++ bot.py
@@ -13,53 +13,100 @@ import os
 import dados_cotacao
 import conversor
 
-#CONFIGURAÇÃO DE VARIAVEIS
-#PLANILHA DE CONTROLE
-#pregao_path = dados.pregao_path
-#excel_read = openpyxl.load_workbook(pregao_path)
-#PREGAO
-pregao_account=dados.pregao_account
-pregao_pass=dados.pregao_pass
-pregao_address=dados.pregao_address
-#SELENIUM
-options = webdriver.ChromeOptions()
-options.add_argument ('--headless')
-options.add_argument('--log-level=3')
-sel_driver = webdriver.Chrome("chromedriver.exe", options=options)
-sel_driver.maximize_window()
-sel_driver.get(pregao_address)
-sel_delay=0.2
+sel_delay=0.5
 
-def sel_enterField(path, text):
-    field = WebDriverWait(sel_driver,10).until(expected_conditions.presence_of_element_located((By.XPATH,path)))
+def sel_enterField(self, path, text):
+    field = WebDriverWait(self.sel_driver,10).until(expected_conditions.presence_of_element_located((By.XPATH,path)))
     field.send_keys(text)
     time.sleep(sel_delay)
 def sel_enterFieldElement(element, text):
     element.send_keys(text)
     time.sleep(sel_delay)
-def sel_buttonClick(path):
-    button = WebDriverWait(sel_driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,path)))
+def sel_buttonClick(self, path):
+    button = WebDriverWait(self.sel_driver,5).until(expected_conditions.element_to_be_clickable((By.XPATH,path)))
     button.click()
-def sel_switchFrame(path):
-    sel_driver.switch_to.default_content()
-    frame = WebDriverWait(sel_driver,10).until(expected_conditions.frame_to_be_available_and_switch_to_it((By.XPATH,path)))
-def sel_mouseHover(path):
-    clickable = WebDriverWait(sel_driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,path)))
-    hover = ActionChains(sel_driver).move_to_element(clickable)
+def sel_switchFrame(self, path):
+    self.sel_driver.switch_to.default_content()
+    frame = WebDriverWait(self.sel_driver,10).until(expected_conditions.frame_to_be_available_and_switch_to_it((By.XPATH,path)))
+def sel_mouseHover(self, path):
+    clickable = WebDriverWait(self.sel_driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,path)))
+    hover = ActionChains(self.sel_driver).move_to_element(clickable)
     hover.perform()
-def sel_getElement(path):
-    el = WebDriverWait(sel_driver,10).until(expected_conditions.presence_of_element_located((By.XPATH,path)))
+def sel_getElement(self, path):
+    el = WebDriverWait(self.sel_driver,10).until(expected_conditions.presence_of_element_located((By.XPATH,path)))
     return el
-def sel_getElements(path):
-    el = WebDriverWait(sel_driver,10).until(expected_conditions.presence_of_all_elements_located((By.XPATH,path)))
+def sel_getElements(self, path):
+    el = WebDriverWait(self.sel_driver,10).until(expected_conditions.presence_of_all_elements_located((By.XPATH,path)))
     return el
-def sel_newWindowClick(path):
-    action = ActionChains(sel_driver).key_down(Keys.SHIFT)
+def sel_newWindowClick(self, path):
+    action = ActionChains(self.sel_driver).key_down(Keys.SHIFT)
     action.perform()
-    action = ActionChains(sel_driver).click(path)
+    action = ActionChains(self.sel_driver).click(path)
     action.perform()
-    action = ActionChains(sel_driver).key_up(Keys.SHIFT)
+    action = ActionChains(self.sel_driver).key_up(Keys.SHIFT)
     action.perform()
+def sel_fechar_popup(self):
+    sel_mainWindow = self.sel_driver.window_handles[0]
+    time.sleep(0.5)
+    sel_windowToClose = self.sel_driver.window_handles[1]
+    self.sel_driver.switch_to.window(sel_windowToClose)
+    self.sel_driver.close()
+    self.sel_driver.switch_to.window(sel_mainWindow)
+
+
+class Robo:
+
+    def iniciar(self):
+        self.configurar_webdriver()
+        self.coletar_credenciais_acessar_sistema()
+        self.acessar_menu_comprasnet()
+        #self.ler_planilha_cotacao()
+        self.fechar_webdriver()
+    
+    def configurar_webdriver(self):
+        self.options = webdriver.ChromeOptions()
+        #self.options.add_argument ('--headless')
+        self.options.add_argument('--log-level=3')
+        self.options.add_argument('--disable-notifications')
+        self.sel_driver = webdriver.Chrome("chromedriver.exe", options=self.options)
+        self.sel_driver.maximize_window()
+        endereco_comprasnet=dados.pregao_address
+        self.sel_driver.get(endereco_comprasnet)
+
+    def coletar_credenciais_acessar_sistema(self):
+        login_comprasnet=dados.pregao_account
+        senha_comprasnet=dados.pregao_pass
+        sel_buttonClick(self,'//*[@id="card0"]/div/div/div/div[2]/button')
+        sel_enterField(self,'//*[@id="txtLogin"]',login_comprasnet)
+        sel_enterField(self,'//*[@id="txtSenha"]', senha_comprasnet)
+        sel_buttonClick(self,'//*[@id="card0"]/div/div/div[2]/div[4]/button[2]')
+        sel_fechar_popup(self)
+        print('Logado no sistema ComprasNet')
+
+    def acessar_menu_comprasnet(self):
+        while (True):
+            sel_switchFrame(self,'/html/frameset/frame[1]')
+            sel_mouseHover(self,'/html/body/div[2]/div[1]')
+            sel_switchFrame(self,'/html/frameset/frameset/frame')
+            time.sleep(0.2)
+            try:
+                sel_buttonClick(self,'/html/body/div[2]/div[4]')
+                break
+            except:
+                self.sel_driver.refresh()
+                sel_fechar_popup(self)
+
+    def ler_planilha_cotacao(self):
+
+        return
+
+    def fechar_webdriver(self):
+        choose = input('>')
+        self.sel_driver.close()
+        return
+
+start = Robo()
+start.iniciar()
 
 def register():
     print('Inserir a planilha de planejamento na pasta que abriu, com o nome COTACAO.xlsx.')
@@ -81,7 +128,6 @@ def register():
 def disputar(pregao, cotados):
     
     return
-
     
 def fight():
     itens_disputa=[]
@@ -174,48 +220,8 @@ def fight():
                         items = table.find_elements_by_xpath('./div')
                         print(str(len(items))+' encerrados: ')
 
+#function_dict={'1':register,'2':fight}
 
 
 
-
-function_dict={'1':register,'2':fight}
-
-#ACESSAR O SISTEMA
-sel_buttonClick('//*[@id="card0"]/div/div/div/div[2]/button')
-sel_enterField('//*[@id="txtLogin"]',pregao_account)
-sel_enterField('//*[@id="txtSenha"]', pregao_pass)
-sel_buttonClick('//*[@id="card0"]/div/div/div[2]/div[4]/button[2]')
-print('Logado no sistema ComprasNet')
-#FECHAR POPUP
-sel_mainWindow = sel_driver.window_handles[0]
-time.sleep(0.5)
-sel_windowToClose = sel_driver.window_handles[1]
-sel_driver.switch_to.window(sel_windowToClose)
-sel_driver.close()
-sel_driver.switch_to.window(sel_mainWindow)
-#ACESSAR SEÇÃO DE PREGÕES
-sel_switchFrame('/html/frameset/frame[1]')
-sel_mouseHover('/html/body/div[2]/div[1]')
-sel_switchFrame('//*[@id="corpo"]/frame')
-time.sleep(0.2)
-try:
-    sel_buttonClick('/html/body/div[1]/div[4]')
-except:
-    sel_driver.refresh()
-    sel_mainWindow = sel_driver.window_handles[0]
-    time.sleep(0.5)
-    sel_windowToClose = sel_driver.window_handles[1]
-    sel_driver.switch_to.window(sel_windowToClose)
-    sel_driver.close()
-    sel_driver.switch_to.window(sel_mainWindow)
-    sel_switchFrame('/html/frameset/frame[1]')
-    sel_mouseHover('/html/body/div[2]/div[1]')
-    sel_switchFrame('//*[@id="corpo"]/frame')
-    time.sleep(0.2)
-    sel_buttonClick('/html/body/div[1]/div[4]')
-print('Proceder com: ')
-print('1 - Cadastrar proposta')
-print('2 - Participar de uma disputa de lances')
-
-choose = input('>')
-function_dict[choose]()
+#function_dict[choose]()
